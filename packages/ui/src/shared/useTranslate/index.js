7ea20996434312bb99d625a6bba84ddb1a36e2d8
Nate Clark <n8@n8io.com>
2019-08-16 14:55:24 -0400

    🚨 Add prettier linting to left-hook

--- packages/ui/src/shared/useTranslate/index.js
+++ packages/ui/src/shared/useTranslate/index.js
@@ -1,4 +1,4 @@
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 export const useTranslate = ({ component, namespace }) => {
   const { t: originalT } = useTranslation(namespace);
