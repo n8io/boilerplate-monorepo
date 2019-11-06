c36e03dc967c97d8c22caf1c13d1d3300f53e922
Nate Clark <n8@n8io.com>
2019-11-05 20:51:06 -0500

    ♻️ Refactor FontLoader to a blocking link

--- packages/ui/src/modules/app/components/App/Providers/FontLoader/index.js
+++ packages/ui/src/modules/app/components/App/Providers/FontLoader/index.js
@@ -3,14 +3,10 @@ import { Font } from 'types/font';
 
 const domTestId = 'FontLoader';
 
-const FontLoader = () => (
-  <style data-testid={domTestId}>
-    @import url(
-    {`https://fonts.googleapis.com/css?family=${encodeURI(
-      Font.numeralsFamilyName
-    )}:400,700|${encodeURI(Font.familyName)}&display=swap`}
-    );
-  </style>
-);
+const href = `https://fonts.googleapis.com/css?family=${encodeURI(
+  Font.numeralsFamilyName
+)}:400,700|${encodeURI(Font.familyName)}`;
+
+const FontLoader = () => <link href={href} rel="stylesheet" />;
 
 export { domTestId, FontLoader };
