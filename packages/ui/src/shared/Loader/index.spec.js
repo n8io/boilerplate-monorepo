2724c485dff06445596ead7bb27282e265112bd9
Nate Clark <n8@n8io.com>
2019-12-05 22:20:35 -0500

    🧪 Add Loader component test

---
+++ packages/ui/src/shared/Loader/index.spec.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'testHelpers';
+import { Loader } from '.';
+
+describe('<Loader/>', () => {
+  const defaultProps = {
+    children: 'CHILDREN',
+  };
+
+  const renderComponent = overrides =>
+    render(<Loader {...defaultProps} {...overrides} />);
+
+  test('renders children', () => {
+    const { container } = renderComponent();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  test('renders size', () => {
+    const size = 100;
+    const { container } = renderComponent({ size });
+
+    expect(container.querySelector(`div[size="${size}"]`)).not.toBeEmpty();
+  });
+});
