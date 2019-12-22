63343e256c9ce25f14c05308a443abd4a2a1df41
Nate Clark <n8@n8io.com>
2019-12-22 14:21:36 -0500

    🔥 Remove Default breakpoint (unnecessary)

--- packages/ui/src/shared/Breakpoints/Default/index.js
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from 'shared/useMediaQuery';
-
-const Default = ({ children }) => {
-  const isNotMobile = useMediaQuery({ minWidth: 0 });
-
-  return isNotMobile ? children : null;
-};
-
-export { Default };
