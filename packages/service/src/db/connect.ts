e67722df9fd106868b07e557054d8aa01515367f
Nate Clark <n8@n8io.com>
2020-01-09 20:32:27 -0500

    ✨ Add db connection retry and backoff logic for service
    Closes #59

--- packages/service/src/db/connect.ts
+++ packages/service/src/db/connect.ts
@@ -1,15 +1,21 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { logFactory } from 'log/logFactory';
+import { multiply } from 'ramda';
 
 const debugLog = logFactory({
   method: 'connect',
   module: 'db',
 });
 
-const connect = async (overrides?: any) => {
-  debugLog('⏲️ Establishing connection to the database...');
+const seconds = multiply(1000);
 
+const wait = (numberOfSeconds: number = 1) =>
+  new Promise(res => setTimeout(res, seconds(numberOfSeconds)));
+
+let attempts: number = 1;
+
+const tryToConnect = async (overrides: any) => {
   const connectionOptions = await getConnectionOptions();
   const options: any = {
     ...connectionOptions,
@@ -17,11 +23,27 @@ const connect = async (overrides?: any) => {
     namingStrategy: new SnakeNamingStrategy(),
   };
 
-  const connection = await createConnection(options);
+  let connection: Connection;
 
-  debugLog('👍 Connected to the database');
+  try {
+    connection = await createConnection(options);
+
+    debugLog('👍 Connected to the database');
+  } catch (err) {
+    await wait(attempts);
+    attempts = attempts + 1;
+
+    debugLog(`🔌 Attempt #${attempts} to connect to the database...`);
+    connection = await tryToConnect(overrides);
+  }
 
   return connection;
+};
+
+const connect = (overrides?: any) => {
+  debugLog('⏳ Establishing connection to the database...');
+
+  return tryToConnect(overrides);
 };
 
 export { connect };
