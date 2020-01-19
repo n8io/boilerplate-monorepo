8052756ede4a61df99d285270e39a072058b32c9
Nate Clark <n8@n8io.com>
2020-01-19 09:07:06 -0500

    🚚 Move Profile component under module components

--- packages/ui/src/modules/profile/index.js
+++ packages/ui/src/modules/profile/index.js
@@ -4,7 +4,7 @@ import { lazy } from 'react';
 const Lazy = lazy(() =>
   import(
     /* webpackChunkName: "profile" */
-    './component'
+    './components'
   ).then(Utils.renameKeys({ Profile: 'default' }))
 );
 
