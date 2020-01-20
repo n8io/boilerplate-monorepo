d020ffa5427889f1bc3b54f178f1da623087c8b8
Nate Clark <n8@n8io.com>
2020-01-20 07:15:19 -0500

    ✨ Add retry logic for expired accessToken

--- packages/ui/src/modules/app/components/App/Providers/GraphQL/link/index.js
+++ packages/ui/src/modules/app/components/App/Providers/GraphQL/link/index.js
@@ -2,8 +2,7 @@ import { ApolloLink } from '@apollo/client';
 import { link as auth } from './auth';
 import { link as http } from './http';
 import { link as onError } from './onError';
-import { link as request } from './request';
 
-const link = ApolloLink.from([auth, request, onError, http]);
+const link = ApolloLink.from([auth, onError, http]);
 
 export { link };
--- packages/ui/src/modules/app/components/App/Providers/GraphQL/link/onError.js
+++ packages/ui/src/modules/app/components/App/Providers/GraphQL/link/onError.js
@@ -1,8 +1,101 @@
+import { Observable } from '@apollo/client';
 import { onError } from 'apollo-link-error';
+import { config } from 'config';
+import { AccessToken } from 'types/accessToken';
+import { history } from 'types/history';
+import { Route } from 'types/route';
 
-const link = onError(({ graphQLErrors, networkError }) => {
-  // eslint-disable-next-line no-console
-  console.error('Received some errors', { graphQLErrors, networkError });
-});
+const UNAUTHENTICATED = 'UNAUTHENTICATED';
+
+const updateToken = ({ token }) => {
+  if (!token) {
+    // eslint-disable-next-line no-console
+    console.error('Could not fetch a new access token, logging out...');
+
+    setTimeout(() => {
+      history.push(Route.LOGOUT.path);
+    }, 1);
+
+    return null;
+  }
+
+  // Set the token, null or not
+  AccessToken.set(token);
+
+  return token;
+};
+
+const fetchAccessToken = () => {
+  const uri = new URL(config.GRAPHQL_URI);
+
+  uri.pathname = 'refresh_token';
+
+  return fetch(uri.href, {
+    credentials: 'include',
+    method: 'POST',
+  })
+    .then(response => response.json())
+    .then(updateToken);
+};
+
+const promiseToObservable = promise => {
+  return new Observable(subscriber => {
+    promise.then(value => {
+      if (subscriber.closed) return;
+
+      if (value) {
+        subscriber.next(value);
+      }
+
+      subscriber.complete();
+    }, subscriber.error);
+  });
+};
+
+const link = onError(
+  // eslint-disable-next-line max-statements
+  ({ forward, graphQLErrors, networkError, operation }) => {
+    if (graphQLErrors) {
+      for (const error of graphQLErrors) {
+        const { extensions, message, path } = error;
+        const { code } = extensions;
+
+        switch (code) {
+          case UNAUTHENTICATED: {
+            const oldHeaders = operation.getContext().headers;
+            const promise = fetchAccessToken();
+
+            return promiseToObservable(promise).flatMap(() => {
+              const token = AccessToken.read();
+
+              operation.setContext({
+                headers: {
+                  ...oldHeaders,
+                  authorization: `bearer ${token}`,
+                },
+              });
+
+              return forward(operation);
+            });
+          }
+          default: {
+            // eslint-disable-next-line no-console
+            console.error(
+              `[${code}]: Failed request for '${path}'. ${message}`,
+              error
+            );
+          }
+        }
+      }
+    }
+
+    if (networkError) {
+      // eslint-disable-next-line no-console
+      console.error(`[NETWORK_ERROR]: ${networkError}`, networkError);
+    }
+
+    return undefined;
+  }
+);
 
 export { link };
