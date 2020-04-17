9d3c67224f8018ac8625f738be48dcd62f3f4f81
Nate Clark <n8@n8io.com>
2020-04-17 15:29:48 -0400

    🔥 Remove email mailer from context

--- packages/service/src/graphql/context.js
+++ packages/service/src/graphql/context.js
@@ -1,11 +1,9 @@
-import { mailer } from 'email/mailer';
 import { Models } from 'models';
 import { readAccessToken } from 'types/auth/selectors';
 
 const context = ({ req, res }) => ({
   Models,
   ip: req.ip,
-  mailer,
   req,
   res,
   user: readAccessToken(req),
