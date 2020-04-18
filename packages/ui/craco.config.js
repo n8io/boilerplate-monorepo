6a13fb8c3ac6da98abba5a72847a680c33bded4e
Nate Clark <n8@n8io.com>
2020-04-18 16:46:32 -0400

    🧹 Code split vendor dependencies

--- packages/ui/craco.config.js
+++ packages/ui/craco.config.js
@@ -15,4 +15,34 @@ module.exports = {
     }),
     mode: ESLINT_MODES.file,
   },
+  webpack: {
+    configure: webpackConfig => ({
+      ...webpackConfig,
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              name(module) {
+                // get the name. E.g. node_modules/packageName/not/this/part.js
+                // or node_modules/packageName
+                const { context } = module;
+
+                const [, packageName] = context.match(
+                  // eslint-disable-next-line prefer-named-capture-group
+                  /[\\/]node_modules[\\/](.*?)([\\/]|$)/u
+                );
+
+                // npm package names are URL-safe, but some servers don't like @ symbols
+                return `npm.${packageName.replace('@', '')}`;
+              },
+              test: /[\\/]node_modules[\\/]/u,
+            },
+          },
+          chunks: 'all',
+          maxInitialRequests: Infinity,
+          minSize: 0,
+        },
+      },
+    }),
+  },
 };
