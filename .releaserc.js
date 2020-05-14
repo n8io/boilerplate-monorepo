03f23844782f68e414bf63280a63a5325b405044
Nate Clark <n8@n8io.com>
2020-05-14 12:47:17 -0400

    fix(build): 💚 Attempt to fix semantic releases

--- .releaserc.js
+++ .releaserc.js
@@ -2,8 +2,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/changelog',
-    '@semantic-release/git',
+    '@semantic-release/github',
   ],
 };
