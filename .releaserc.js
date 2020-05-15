bc77fb45cba5ede97388276c38ae895ac5aaceb1
Nate Clark <n8@n8io.com>
2020-05-15 15:02:33 -0400

    fix: 🔥 Remove extra build stuffs around version bumping
    - ⬅️ Rollback package.json's to v1.0.0

--- .releaserc.js
+++ .releaserc.js
@@ -1,14 +1,3 @@
 module.exports = {
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    [
-      '@semantic-release/github',
-      {
-        failComment: false,
-        successComment: false,
-      },
-    ],
-  ],
+  extends: 'semantic-release-monorepo',
 };
--- packages/ui-common/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# Change Log
-
-All notable changes to this project will be documented in this file.
-See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
-
-## [1.0.3](https://github.com/n8io/boilerplate-monorepo/compare/v1.0.0...v1.0.3) (2020-05-14)
-
-**Note:** Version bump only for package @boilerplate-monorepo/ui-common
