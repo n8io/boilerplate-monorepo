83e4d7cac07760096ba1d16c05c0a5ed8665c545
Nate Clark <n8@n8io.com>
2020-05-16 13:44:56 -0400

    chore(a11y): 🔥 Remove `react-axe` tool in dev mode
    It was causing a lot of incorrect errors to pollute the console

--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -1,23 +1,10 @@
-import { config } from 'config';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'modules/app';
 import { initializeTranslations } from './i18n';
 
-const { isProduction } = config;
-
 initializeTranslations();
 
 const rootEl = document.getElementById('root');
 
-if (isProduction) {
-  ReactDOM.render(<App />, rootEl);
-} else {
-  import(
-    /* webpackChunkName: "devTools" */
-    'devTools'
-  ).then(async ({ load }) => {
-    await load();
-    ReactDOM.render(<App />, rootEl);
-  });
-}
+ReactDOM.render(<App />, rootEl);
