1789a35fdeadc367aca99298fc3283c5342ff953
Nate Clark <n8@n8io.com>
2020-06-08 13:19:35 -0400

    ci: 💚 CI builds for master merges

--- packages/ui/src/modules/app/components/App/Layout/Meta/index.js
+++ packages/ui/src/modules/app/components/App/Layout/Meta/index.js
@@ -4,12 +4,21 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 
 const Meta = () => {
-  const { RELEASE } = config;
+  const { COMMIT_HASH, COMMIT_MESSAGE, RELEASE } = config;
 
   return (
     <Helmet>
       <meta name="theme-color" content={Color.PRIMARY} />
       <meta name="version" content={RELEASE} />
+      {COMMIT_HASH && (
+        <meta
+          content={COMMIT_HASH}
+          data-testid="commit"
+          message={COMMIT_MESSAGE}
+          name="commit"
+          url={`https://github.com/n8io/boilerplate-monorepo/commit/${COMMIT_HASH}`}
+        />
+      )}
     </Helmet>
   );
 };
--- packages/ui/src/modules/app/components/App/Layout/Meta/index.spec.js
+++ packages/ui/src/modules/app/components/App/Layout/Meta/index.spec.js
@@ -4,18 +4,51 @@ import { render } from 'testHelpers';
 import { Meta } from '.';
 
 describe('<Meta/>', () => {
+  const defaultConfig = {
+    COMMIT_MESSAGE: 'COMMIT_MESSAGE',
+    RELEASE: 'RELEASE',
+  };
+
   const defaultProps = {};
 
   const renderComponent = (overrides) =>
     render(<Meta {...defaultProps} {...overrides} />);
 
   test('renders properly', () => {
-    td.replace(Config, 'config', {
-      RELEASE: 'RELEASE',
-    });
+    td.replace(Config, 'config', defaultConfig);
 
     const { container } = renderComponent();
 
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  describe('when there is commit metadata', () => {
+    const COMMIT_HASH = 'COMMIT_HASH';
+
+    test('renders release metadata', () => {
+      td.replace(Config, 'config', {
+        ...defaultConfig,
+        COMMIT_HASH,
+      });
+
+      const { queryByTestId } = renderComponent();
+
+      expect(queryByTestId('commit')).not.toBeNull();
+    });
+  });
+
+  describe('when there is NOT commit metadata', () => {
+    const COMMIT_HASH = undefined;
+
+    test('renders release metadata', () => {
+      td.replace(Config, 'config', {
+        ...defaultConfig,
+        COMMIT_HASH,
+      });
+
+      const { queryByTestId } = renderComponent();
+
+      expect(queryByTestId('commit')).toBeNull();
+    });
   });
 });
