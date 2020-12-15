30d70c13601db56b390789baf14ecc8b1e855d43
Nate Clark <n8@n8io.com>
2020-12-14 21:53:37 -0500

    chore: ♻️ Extract server startup

--- packages/service/src/start.js
+++ packages/service/src/start.js
@@ -1,13 +1,8 @@
 import { make as makeApp } from 'app';
 import { make as makeCache } from 'cache';
-import { config } from 'config';
 import { makeConnection } from 'db';
-import { once } from 'events';
 import { make as makeGraphqlServer } from 'server';
-import { addListeners as addServerStopListeners } from 'stop';
 import { Telemetry } from 'types/telemetry';
-
-const { PORT } = config;
 
 const start = async ({ app, cache, connection }) => {
   Telemetry.init();
@@ -21,26 +16,11 @@ const start = async ({ app, cache, connection }) => {
     cache: actualCache,
   });
 
-  const expressServer = graphqlServer.listen({ port: PORT }, () => {
-    const msg = `🚀 GraphQL server started @ https://local.host:${PORT}/graphql`;
-    const bookend = '='.repeat(msg.length - 1);
-
-    // eslint-disable-next-line no-console
-    console.log([bookend, msg, bookend].join('\n'));
-  });
-
-  await once(expressServer, 'listening');
-
-  addServerStopListeners({
-    cache: actualCache,
-    connection: actualConnection,
-  });
-
   return {
     app: actualApp,
     cache: actualCache,
     connection: actualConnection,
-    server: expressServer,
+    server: graphqlServer,
   };
 };
 
