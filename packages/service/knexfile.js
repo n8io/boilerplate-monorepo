e4cb9b37cdaff1d9d4f619fb580ae437429e154c
Nate Clark <n8@n8io.com>
2020-12-29 16:44:50 -0500

    build(aws): ✨ Lambda-fy migrations

--- packages/service/knexfile.js
+++ packages/service/knexfile.js
@@ -7,6 +7,7 @@ const {
   DATABASE_MIGRATION_TABLE_NAME,
   DATABASE_URL,
   DATABASE_USE_SSL,
+  NODE_ENV,
   // eslint-disable-next-line no-process-env
 } = process.env;
 
@@ -18,6 +19,12 @@ if (!DATABASE_URL) {
   process.exit(1);
 }
 
+const distMigrations = path.join(__dirname, 'dist/migrations');
+const srcMigrations = path.join(__dirname, 'src/migrations');
+
+// eslint-disable-next-line no-sync
+const directory = NODE_ENV === 'production' ? distMigrations : srcMigrations;
+
 const defaults = {
   client: 'pg',
   connection: {
@@ -25,7 +32,7 @@ const defaults = {
     ssl: DATABASE_USE_SSL ? { rejectUnauthorized: false } : false,
   },
   migrations: {
-    directory: path.join(__dirname, 'src/migrations'),
+    directory,
     schema: DATABASE_MIGRATION_SCHEMA || 'public',
     tableName: DATABASE_MIGRATION_TABLE_NAME || 'migrations',
   },
